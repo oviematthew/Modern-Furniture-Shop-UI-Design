79295853629573c2fd503d3e94c303b3c4c08b26
oviematthew <90224141+oviematthew@users.noreply.github.com>
2022-08-23 23:42:34 +0100

    Add files via upload
    -Added Scroll to top button
    -Added Dark mode with custom colors for each mode

--- myjs.js
+++ myjs.js
@@ -1,6 +1,42 @@
-$('.owl-carousel').owlCarousel({
-    margin:10,
-    loop:true,
-    autoWidth:true,
-    items:4
-})
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
+var icon = document.getElementById ("icon");
+
+icon.onclick=function(){
+
+    document.body.classList.toggle("dark-mode");
+    if(document.body.classList.contains("dark-mode")){
+        icon.src="/images/sun-regular.png";
+       
+    }else{
+        icon.src="/images/moon-regular.svg";
+    }
+                                          I
+
+}
